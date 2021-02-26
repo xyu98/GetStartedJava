8f4876855ebd0a9b1d5d9e2d733c3a9ead9d2c71
xyu98 <18810509729@163.com>
2021-02-27 00:14:52 +0800

    Update InterfaceTest

--- pak/test/InterfaceTest.java
+++ pak/test/InterfaceTest.java
@@ -2,9 +2,12 @@ package pak.test;
 
 public class InterfaceTest {
 	public static void main(String[] args) {
-		Customer customer1 = new Customer();
-		Customer customer2 = new Customer();
-		
+		// create cook object
+		FoodMenu cook1 = new EasternCook();  // 接口无法new,只能new接口的实现类(多态)
+		// create customer object
+		Customer customer1 = new Customer(cook1); // customer有成员变量FoodMenu,把实际的cook1传进去★★★
+		// the customer taking order
+		customer1.order();
 	}
 }
 
@@ -15,16 +18,36 @@ interface FoodMenu {
 }
 
 class Customer {
-	// Customer调用的是Menu接口
-	// 不直接调用EasternCook和WesternCook 降低类与类之间的耦合度
+	// Customer调用的是FoodMenu接口
+	// EasternCook和WesternCook是FoodMenu接口的实现类
+	// 不直接调用实现类，以降低代码耦合度
 	// EasternCook ec;
 	// WesternCook wc;
-	FoodMenu menu;
+	private FoodMenu menu; //养成封装的习惯,同时要提供访问和修改方法
 	
+	// constructor
+	public Customer() {
+	}
 	
-	public void order(FoodMenu menu) {
-		System.out.println();
-		
+	// 这里必须要有带参数的构造方法,目的是给menu赋一个值
+	public Customer(FoodMenu menu) {
+		this.menu = menu;
+	}
+	
+	// setter and visitor
+	public void setFoodMenu(FoodMenu menu) {
+		this.menu = menu;
+	}
+	
+	public FoodMenu getFoodMenu() {
+		return menu;
+	}
+	
+	public void order() {
+		FoodMenu menu = this.getFoodMenu(); //类的内部也可以直接访问
+		menu.dumplings();  // 如果没有带参数构造方法的话,成员变量menu是空的,这里会出现空指针异常★★★
+		menu.chips();
+		menu.salad();
 	}
 }
 
